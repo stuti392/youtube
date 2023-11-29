58e3d6955eb3b919fa1969db8c1a79aa4ca5bc70
stuti392 <sstuti84@gmail.com>
2023-11-29 19:43:22 +0530

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,65 @@
+import {useState, useEffect} from 'react';
+import {Box, Stack, Typography} from '@mui/material';
+
+
+import { fetchFromAPI } from '../utils/fetchFeomAPI';
+import Sidebar from './Sidebar';
+import Videos from './Videos';
+
+
+const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New')
+
+  useEffect (() => {
+    fetchFromAPI( `search?part=snippet&q=${selectedCategory}`); 
+  }, [selectedCategory])
+
+
+
+  return (
+
+    <Stack
+       sx={{flexDirection:{sx:"column", md:"row"}}}>
+
+      <Box sx={{height: {sx: 'auto', md:'92vh'}, borderRight: '1px solid #3d3d3d', px: {sx:0, md:2}}}>
+
+        {/* <Sidebar
+        
+        selectedCategory = {selectedCategory}
+        setSelectedCategory = {setSelectedCategory}
+        
+        /> */}
+
+        <Sidebar
+         selectedCategory = {selectedCategory}
+         setSelectedCategory = {setSelectedCategory}/>
+      
+
+        <Typography className='copyright' variant='body2' sx={{mt:1.5,color:'#fff'}}>
+
+          Copyright 2022 JSM Media
+
+        </Typography>
+      </Box>  
+
+      <Box p={2} sx={{overflow:'auto', height:'90vh', flex:2}}>
+
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{color:'white'}}>
+          {selectedCategory} <span style={{color:'#F31503'}}>videos</span>
+        </Typography>
+
+        {/* <Videos videos={[]} /> */}
+        <Videos videos={[]}/>
+
+
+
+      </Box>
+
+    </Stack>
+
+
+  )
+}
+
+export default Feed
